a0da803c22982f64742e9ef66f397ef59cb49dc8
xiejunxiang <1923213756@qq.com>
2026-02-03 16:25:51 +0800

    Fix install method

--- Formula/nine1bot.rb
+++ Formula/nine1bot.rb
@@ -8,14 +8,6 @@ class Nine1bot < Formula
     on_arm do
       url "https://github.com/contrueCT/nine1bot/releases/download/v1.0.0/nine1bot-darwin-arm64-v1.0.0.tar.gz"
       sha256 "b7e6fccfcd519a3308363d1662c606824f9c3f799c0da12b2ee789e53cc0e0d8"
-
-      def install
-        bin.install "nine1bot-darwin-arm64/nine1bot" => "nine1bot"
-        prefix.install "nine1bot-darwin-arm64/bin" => "libexec/bin"
-        prefix.install "nine1bot-darwin-arm64/skills" => "skills"
-        prefix.install "nine1bot-darwin-arm64/web" => "web"
-        prefix.install "nine1bot-darwin-arm64/scripts" => "scripts"
-      end
     end
   end
 
@@ -23,27 +15,30 @@ class Nine1bot < Formula
     on_arm do
       url "https://github.com/contrueCT/nine1bot/releases/download/v1.0.0/nine1bot-linux-arm64-v1.0.0.tar.gz"
       sha256 "e5a5eee6049b2815bca607f3129400912c9dda9e06abf3c12152a56e82dc7fab"
-
-      def install
-        bin.install "nine1bot-linux-arm64/nine1bot" => "nine1bot"
-        prefix.install "nine1bot-linux-arm64/bin" => "libexec/bin"
-        prefix.install "nine1bot-linux-arm64/skills" => "skills"
-        prefix.install "nine1bot-linux-arm64/web" => "web"
-        prefix.install "nine1bot-linux-arm64/scripts" => "scripts"
-      end
     end
     on_intel do
       url "https://github.com/contrueCT/nine1bot/releases/download/v1.0.0/nine1bot-linux-x64-v1.0.0.tar.gz"
       sha256 "c0734f257fe617a5d01e087a62813517e0e40210faeecc7098bcedaba2be7287"
-
-      def install
-        bin.install "nine1bot-linux-x64/nine1bot" => "nine1bot"
-        prefix.install "nine1bot-linux-x64/bin" => "libexec/bin"
-        prefix.install "nine1bot-linux-x64/skills" => "skills"
-        prefix.install "nine1bot-linux-x64/web" => "web"
-        prefix.install "nine1bot-linux-x64/scripts" => "scripts"
-      end
     end
+  end
+
+  def install
+    # 根据平台确定目录名
+    if OS.mac? && Hardware::CPU.arm?
+      dir = "nine1bot-darwin-arm64"
+    elsif OS.linux? && Hardware::CPU.arm?
+      dir = "nine1bot-linux-arm64"
+    elsif OS.linux? && Hardware::CPU.intel?
+      dir = "nine1bot-linux-x64"
+    else
+      dir = "nine1bot-darwin-arm64"
+    end
+
+    bin.install "#{dir}/nine1bot" => "nine1bot"
+    prefix.install "#{dir}/bin" => "libexec/bin"
+    prefix.install "#{dir}/skills" => "skills"
+    prefix.install "#{dir}/web" => "web"
+    prefix.install "#{dir}/scripts" => "scripts"
   end
 
   def caveats
